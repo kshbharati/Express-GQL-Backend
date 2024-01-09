19265a44533d6d6b5c6b869509e3a0f507bb9668
Kshitiz Bharati <kshitizxox@hotmail.com>
2024-01-09 18:53:16 +1100

    Generalized errors and messages

--- src/ErrorHelpers.ts
+++ src/ErrorHelpers.ts
@@ -1,12 +1,29 @@
 import { GraphQLError, GraphQLErrorExtensions } from "graphql/error/GraphQLError";
 
+export type ServerErrorOptions = {message:string, status?:string};
+
+const ErrorFormat = (message:string, status?:string):ServerErrorOptions => ({message:message, status:status});
+
 export enum ErrorCode {
     DUPLICATE_ENTRY="DUPLICATE_ENTRY",
     INTERNAL_SERVER_ERROR="INTERNAL_SERVER_ERROR",
     NOT_FOUND="NOT_FOUND",
     BAD_USER_INPUT="BAD_USER_INPUT",
     FORBIDDEN="FORBIDDEN",
-    ALREADY_LOGGED_IN="ALREADY_LOGGED_IN"
+    ALREADY_LOGGED_IN="ALREADY_LOGGED_IN",
+    INVALID="INVALID"
+}
+
+export const ServerError = {
+    DUPLICATE_ENTRY: ErrorFormat("Provided entry already exists. Please ensure unique fields are changed",ErrorCode.DUPLICATE_ENTRY),
+    INTERNAL_SERVER_ERROR: ErrorFormat("Server encountered unexpected Error",ErrorCode.INTERNAL_SERVER_ERROR),
+    NOT_FOUND: ErrorFormat("Query not found",ErrorCode.NOT_FOUND),
+    BAD_USER_INPUT: ErrorFormat("Provided input is not valid or does not match.", ErrorCode.BAD_USER_INPUT),
+    FORBIDDEN: ErrorFormat("Not Allowed",ErrorCode.FORBIDDEN),
+    LOGGED_IN: ErrorFormat("User is already logged.",ErrorCode.ALREADY_LOGGED_IN),
+    NOT_LOGGED_IN: ErrorFormat("Either you haven't logged in or the authentication has Expired. Please login to continue", ErrorCode.FORBIDDEN),
+    TOKEN_INVALID: ErrorFormat("Token Invalid or Expired", ErrorCode.INVALID),
+    
 }
 
 export const GQLErrorOptions = (
@@ -18,6 +35,6 @@ export const GQLErrorOptions = (
         }
     );
 
-export const GQLError = (message:string, code?:string, argumentName?:string):GraphQLError => {
-    return new GraphQLError(message, GQLErrorOptions(code, argumentName));
+export const GQLError = (error: ServerErrorOptions,  argumentName?:string):GraphQLError => {
+    return new GraphQLError(error.message, GQLErrorOptions(error.status, argumentName));
 }
--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -1,16 +1,16 @@
 import "reflect-metadata";
-import { PrivateUser, PublicUser} from "@root/schema/User";
+import { PublicUser} from "@root/schema/User";
 import { Arg, Ctx, Field, InputType, Mutation,Query, Resolver } from "type-graphql";
 import { AppContext } from "@root/context";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
-import { GraphQLError, GraphQLErrorExtensions } from "graphql";
 import UserResponse from '@root/APIResponse'
 import { hashPass, comparePass } from "@root/PassMgmt";
 import { getLoggedUser } from "@root/SessionMgmt";
 import { decodeToken, generateToken } from "@root/TokenMgmt";
 import { User } from "@prisma/client";
 import { IsEmail } from "class-validator";
-import { ErrorCode, GQLError, GQLErrorOptions } from "@root/ErrorHelpers";
+import { ErrorCode, GQLError, ServerError } from "@root/ErrorHelpers";
+import { GraphQLError } from "graphql";
 
 @InputType()
 class UserCreateInput {
@@ -75,8 +75,7 @@ export class UserResolver {
                 },
             });
 
-            if (!user) throw GQLError('Server Encountered UnExpected Problem. Please Try Again',ErrorCode.INTERNAL_SERVER_ERROR);
-
+            if (!user) throw GQLError(ServerError.INTERNAL_SERVER_ERROR,"Database");
             return {
                 status: "200",
                 message: "User created with email " + user.email,
@@ -84,25 +83,27 @@ export class UserResolver {
             
         } catch (e: any) {
             if (e instanceof PrismaClientKnownRequestError) {
-                if (e.code === "P2002") throw GQLError("Value is already being used.", ErrorCode.DUPLICATE_ENTRY,'email');
+                if (e.code === "P2002") throw GQLError(ServerError.DUPLICATE_ENTRY,"user");
             }
-            throw GQLError(e.response?.data?.message)
+            throw new GraphQLError(e.response?.data?.message)
         }
     }
 
-    //TODO Update this function and User field to include change to be depending on User Type
     @Mutation((type) =>UserResponse)
     async updateEmail(
         @Arg("data") data:UserUpdateEmail,
         @Ctx() ctx:AppContext
     ): Promise<UserResponse>
     {
-        const loggedUser:User = await getLoggedUser(ctx) as User;
+        let sessionResponse = await getLoggedUser(ctx);
+        if (sessionResponse instanceof GraphQLError) throw sessionResponse;
+
+        const loggedUser = sessionResponse as User;
 
         if (!loggedUser)
-            throw GQLError("Either you haven't logged in or the authentication has Expired. Please login to continue",ErrorCode.FORBIDDEN,"token");
+            throw GQLError(ServerError.NOT_LOGGED_IN);
         
-        if(loggedUser.email===data.newEmail) throw GQLError("Same Email Provided.",ErrorCode.DUPLICATE_ENTRY);
+        if(loggedUser.email===data.newEmail) throw GQLError(ServerError.BAD_USER_INPUT,"email");
 
         try{
             const user = await ctx.prisma.user.update({
@@ -117,7 +118,7 @@ export class UserResolver {
                     email: true,
                 },
             });
-            if (!user) throw GQLError("User doesn't exist",ErrorCode.NOT_FOUND,'email')
+            if (!user) throw GQLError(ServerError.INTERNAL_SERVER_ERROR);
 
             await ctx.redis.del(loggedUser.email);
 
@@ -134,7 +135,7 @@ export class UserResolver {
                 token:token
             };
         }catch(err:any){
-            throw GQLError(err.response?.data?.message)
+            throw new GraphQLError(err.response?.data?.message)
         }
 
 
@@ -146,17 +147,16 @@ export class UserResolver {
         @Arg("data") data:UserUpdatePassword,
         @Ctx() ctx:AppContext
     ):Promise<UserResponse>{
-        const loggedUser: User = (await getLoggedUser(ctx)) as User;
+        let sessionResponse = await getLoggedUser(ctx);
+        if (sessionResponse instanceof GraphQLError) throw sessionResponse;
+
+        const loggedUser = sessionResponse as User;
 
         if (!loggedUser)
-            throw GQLError(
-                "Either you haven't logged in or the authentication has Expired. Please login to continue",
-                ErrorCode.FORBIDDEN,
-                "token"
-            );
+            throw GQLError(ServerError.NOT_LOGGED_IN);
         
             try{
-                if(await !comparePass(loggedUser.password, data.oldPassword)) throw GQLError("Old Password Doesn't match", ErrorCode.BAD_USER_INPUT,'old_password');
+                if(await !comparePass(loggedUser.password, data.oldPassword)) throw GQLError(ServerError.BAD_USER_INPUT, "password");
 
                 const updatedUser = await ctx.prisma.user.update({
                     where:{
@@ -175,31 +175,32 @@ export class UserResolver {
                     message: 'Password changed successfully',
                 };
             }catch(err:any){
-                throw GQLError(err.response?.data?.message);
+                throw new GraphQLError(err.response?.data?.message);
             }
     }
+    
 
     @Mutation((type) => UserResponse)
     async Login(
         @Arg("data") data: UserLoginInput,
         @Ctx() ctx: AppContext
     ): Promise<UserResponse> {
-        /* Deletes the old token if there are any*/
+        const loggedUser = await getLoggedUser(ctx);
+        if (loggedUser instanceof GraphQLError) throw loggedUser;
 
-        const loggedUser:User = await getLoggedUser(ctx) as User;
+        if (loggedUser)
+            throw GQLError(ServerError.LOGGED_IN);
         
-        if(loggedUser) throw GQLError("User Already Logged In. Logout to login to another account",ErrorCode.ALREADY_LOGGED_IN);
-
         const user = await ctx.prisma.user.findUnique({
             where: {
                 email: data.email,
             },
         });
 
-        if (!user) throw GQLError("One or more Fields Do not match",ErrorCode.BAD_USER_INPUT,'email or password');
+        if (!user) throw GQLError(ServerError.BAD_USER_INPUT,'email');
 
         if (await !comparePass(data.password, user.password))
-            throw GQLError("One or more Fields Do not match",ErrorCode.BAD_USER_INPUT,"email or password");
+            throw GQLError(ServerError.BAD_USER_INPUT, "password");
 
         const token = await generateToken({
             id: user.id,
@@ -217,7 +218,7 @@ export class UserResolver {
 
     @Mutation((type) => UserResponse)
     async LogOut(@Ctx() ctx: AppContext): Promise<UserResponse> {
-        if(!ctx.token) throw GQLError("User Token not provided.");
+        if(!ctx.token) throw GQLError(ServerError.NOT_FOUND,"token");
 
         const token:User = await decodeToken(ctx.token as string) as User;
 
@@ -231,12 +232,10 @@ export class UserResolver {
     @Query(() => UserResponse)
     async allUsers(@Ctx() ctx: AppContext): Promise<UserResponse> {
         const loggedUser = await getLoggedUser(ctx);
+        if(loggedUser instanceof GraphQLError) throw loggedUser;
 
-        if (!loggedUser)
-            throw GQLError(
-                "Either you haven't logged in or the authentication has Expired. Please login to continue",ErrorCode.FORBIDDEN
-            );
-
+        if (!loggedUser) throw GQLError(ServerError.NOT_LOGGED_IN);
+        
         return {
             status: "200",
             data: await ctx.prisma.user.findMany(),
